b6e87a1b4773804846d29c098decd9691bc7f588
Sarthak Ahir <sarthakdahir@gmail.com>
2026-02-15 11:21:47 +0530

    {Add Functionality}

--- currencyConvertor/src/hooks/useCurrencyInfo.js
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,17 +1,18 @@
-import {useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    const [data, setData] =useState({});
-    useEffect(()=> {
-        fetch(`https://v6.exchangerate-api.com/v6/
-            2805b6037a46e153058c031a/latest/${currency}`)
-            
-            .then(res => res.json())
-            .then((res) =>setData(res[currency]));
-            console.log(data);
-    } , [currency])
-    console.log(data);
-    return data;
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://v6.exchangerate-api.com/v6/2805b6037a46e153058c031a/latest/${currency}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.conversion_rates);   
+        console.log(data);
+      });
+  }, [currency]);
+
+  return data;
 }
 
 export default useCurrencyInfo;
