db943807f6dd20fc8e0c35472f3a450ef233336b
Sarthak Ahir <sarthakdahir@gmail.com>
2026-06-30 19:11:38 +0530

    redux todo

--- 10reduxtoolkittodo/src/App.jsx
+++ 10reduxtoolkittodo/src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   
@@ -6,6 +8,8 @@ function App() {
   return (
     <>
     <h1>Learn about redux tool kit</h1>
+    <AddTodo />
+    <Todos />
     </>
   )
 }
