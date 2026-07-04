33ac14e5e9e5f7f13be5da4935079043394e0621
Sarthak Ahir <sarthakdahir@gmail.com>
2026-07-04 19:57:09 +0530

    {Add tailwind and add some components in app }

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,14 +1,39 @@
 import './App.css'
+import  {useState, useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from "./store/authSlice"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return ( 
-    <>
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-      <div>A blog app with appwrite</div>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally()
+  }, []);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
