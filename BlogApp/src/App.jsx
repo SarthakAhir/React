ea0d004217ec4df56f8ead797157bb7279de1137
Sarthak Ahir <sarthakdahir@gmail.com>
2026-07-10 20:15:21 +0530

    {Header} - {add nav items in header by checking status}

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -3,7 +3,7 @@ import  {useState, useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import authService from './appwrite/auth'
 import {login, logout} from "./store/authSlice"
-import { Footer, Header } from './components'
+import { Footer, Header } from './components/index'
 
 function App() {
 
