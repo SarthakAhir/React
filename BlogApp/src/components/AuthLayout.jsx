2e759c5bd2b052f434a3bf74c60ba5c423fd79d3
Sarthak Ahir <sarthakdahir@gmail.com>
2026-07-15 18:21:04 +0530

    {Auth layout where we are creating protected route}

---
+++ BlogApp/src/components/AuthLayout.jsx
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+
+export default function Protected({children, authentication = true}) {
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector((state)=> state.auth.status)
+
+    useEffect(()=>{
+
+        if(authentication && authStatus !== authentication){
+            navigate('/login')
+        }
+        else if(!authentication && authStatus !== authentication){
+            navigate('/')
+        }
+
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+  return (
+    loader ? <h1>Loadeing...</h1> : <>{children}</>
+  )
+}
