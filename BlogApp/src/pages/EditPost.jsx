3d6b8d32c89c92dafe631c440634ba7dca9a1725
Sarthak Ahir <sarthakdahir@gmail.com>
2026-07-17 18:25:29 +0530

    {Create Page} - {Edit post}

---
+++ BlogApp/src/pages/EditPost.jsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react'
+import {Container, PostFrom} from '../components/index'
+import appwriteService from '../appwrite/config'
+import { useParams, useNavigate } from 'react-router-dom'
+
+function EditPost() {
+
+    const [post, setPost] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(slug){
+            appwriteService.getPost(slug).then((post)=>{
+                if(post){
+                    setPost(post)
+                }
+            })
+        }
+        else{
+                navigate('/')
+           }
+    }, [slug, navigate])
+
+  return post ? (
+    <div className="py-8">
+        <Container>
+            <PostFrom post={post} />
+        </Container>
+    </div>
+  ): null
+
+}
+
+export default EditPost
